e18c8378eacc2042f66032ff3ca5c458e6555ebf
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-04-12 22:03:18 +0900

    Create if-elseif-else쓰기.java

---
+++ src/if-elseif-else쓰기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class baekjoon1330 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        if (a == b) {
+            System.out.println("==");
+        } else if (a > b) {
+            System.out.println(">");
+        } else {
+            System.out.println("<");
+        }
+    }
+}
