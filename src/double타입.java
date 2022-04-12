b2b07f7607501ffb6f20d5f1861bdbf988f83351
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-04-12 21:52:32 +0900

    Create double타입.java
    double = long

---
+++ src/double타입.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class baekjoon1008 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextInt();
+        double b = input.nextInt();
+        System.out.println(a/b);
+    }
+}
